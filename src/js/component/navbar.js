f7370d97ca05e914c28a4d873ccfb8154bf6473f
Mauricio Lizcano <mauriciolizcano25531@gmail.com>
2021-01-30 20:12:57 -0400

    first commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,48 @@
 import React from "react";
+import { Context } from "../store/appContext.js";
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+export class Navbar extends React.Component {
+	state = {
+		isOpen: false
+	};
+
+	toggleOpen = () => this.setState({ isOpen: !this.state.isOpen });
+
+	render() {
+		const menuClass = `dropdown-menu${this.state.isOpen ? " show" : ""}`;
+		return (
+			<div className="navbar">
+				<h2 className="text-left">Star Wars</h2>
+				<div>
+					<Link to="/">
+						<button className="btn btn-primary w-100">Home</button>
+					</Link>
+					<div className="dropdown dropleft" onClick={this.toggleOpen}>
+						<a
+							className="btn btn-primary dropdown-toggle"
+							href="#"
+							role="button"
+							id="dropdownMenuLink"
+							data-toggle="dropdown"
+							aria-haspopup="true"
+							aria-expanded="false">
+							Favorites
+						</a>
+						<div className={menuClass} aria-labelledby="dropdownMenuLink">
+							<Context.Consumer>
+								{({ store }) => {
+									return store.favorites.map((fav, ind) => (
+										<a key={ind} className="dropdown-item">
+											{fav.name}
+										</a>
+									));
+								}}
+							</Context.Consumer>
+						</div>
+					</div>
+				</div>
 			</div>
-		</nav>
-	);
-};
+		);
+	}
+}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,46 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.scss";
+import PeopleCard from "../component/peopleCard.js";
+import PlanetCard from "../component/planetCard.js";
+import { Context } from "../store/appContext.js";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div className="container-fluid text-center mt-5">
+				<div className="row character">
+					<h2> Characters </h2>
+					<div className="card-columns d-flex justify-content-between">
+						<Context.Consumer>
+							{({ store }) => {
+								return store.detailsPeoples.map((elem, index) => {
+									return (
+										<PeopleCard name={elem.name} gender={elem.gender} key={index} camel={index} />
+									);
+								});
+							}}
+						</Context.Consumer>
+					</div>
+				</div>
+				<div className="row character">
+					<h2> Planets </h2>
+					<div className="card-columns d-flex justify-content-between">
+						<Context.Consumer>
+							{({ store }) => {
+								return store.detailsPlanets.map((elem, index) => {
+									return (
+										<PlanetCard name={elem.name} gender={elem.gender} key={index} camel={index} />
+									);
+								});
+							}}
+						</Context.Consumer>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
